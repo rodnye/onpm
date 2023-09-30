5698deea60aba830746b6e4ac5ab98d9ab398fe7
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-09-30 03:14:25 -0400

    feat: added get flags values array

--- src/helpers/args.js
+++ src/helpers/args.js
@@ -4,43 +4,52 @@ const {red, yellow} = require("colors/safe");
 /** 
  * Process a arguments array
  *
- * @param {process.argv} argv
+ * @param {process.argv} _argv
  * @param {string[]} availableFlags
- * @param option.ignoreFlagsError {boolean} ignore Flags Error
+ * @param {boolean} option.ignoreFlagsError - ignore Flags Error
  */
 function processArgs (argv, availableFlags, options = {}) {
     
+    // Output object
     const data = {
         args: [],
-        arg0: null
+        arg0: null,
+        "": [],
     };
     
+    // Remove unnecesary default params
     argv = argv.slice(2);
+    
+    // Loop params
+    let currentFlag = "";
     while (argv.length) {
         const arg = argv.shift();
         
         if (arg.indexOf("-") === 0) {
-            // is an option flag!
-            const _split = arg.split("=");
-            const flag = _split[0];
-            const value = _split[1] || true;
-            
+            // Is an option flag! 
+            const flag = arg;
+            data[flag] = [];
+            currentFlag = arg;
+
             if (!options.ignoreFlagsError && !availableFlags.includes(flag)) {
-                // flag not found
+                // Flag not found
                 console.error(
                     red.bold("fatal: ") + "Unknown or unexpected option \"" + arg + "\"\n" +
                     yellow("hint: Use -h or --help flag to see the commands and their options")
                 );
                 process.exit();
             }
-            data[flag] = value;
+
+
         }
-        
         else {
-            // is a parameter!
+            // Is a parameter or value !
             data.args.push(arg);
+            data[currentFlag].push(arg);
         }
     }
+    
+    // Set initial param
     data.arg0 = data.args[0];
     
     
