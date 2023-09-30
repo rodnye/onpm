f4064e33f2a76ae0a8b1879643ffbd91915ae6c8
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-09-30 03:12:46 -0400

    feat: added module name parser helper

---
+++ src/helpers/module-name-parser.js
@@ -0,0 +1,31 @@
+
+/**
+ * Parse the module name notation
+ * 
+ * @param {string} input - a module name notation
+ * @return {{name: string, version: string}}
+ */
+function parseModuleName (input) {
+    
+    let nameSplit = input.split("@");
+    let name;
+    let version;
+        
+    if (nameSplit[0]) {
+        // Syntax `module@version`
+        name = nameSplit[0];
+        version = nameSplit[1];
+    }
+    else {
+        // Syntax `@folder/module@version`
+        name = "@" + nameSplit[1];
+        version = nameSplit[2];
+    }
+
+    return {
+        name,
+        version,
+    }
+}
+
+module.exports = parseModuleName;
