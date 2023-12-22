528501f696510610df2c6a085fc763f0c57bd4c6
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-12-22 02:01:59 -0500

    feat: stylishized loading text

--- src/logic/loading.js
+++ src/logic/loading.js
@@ -1,29 +1,39 @@
-/**
- * create and update a loading bar in console 
- * [#######.....] 
- */
-const {yellow} = require("colors/safe");
 
+const {gray, bgGray, white, bgWhite} = require("colors/safe")
+
+/**
+* Class representing a loading text bar
+*/
 class LoadingTextBar {
+    /**
+    * Create a loading text bar
+    * @param {number} speed - The speed of the progress
+    */
     constructor (speed) {
-        this.speed = speed || 0;
-        this.progress = 0;
+        this.speed = speed || 0; // The speed of the progress, defaults to 0
+        this.progress = 0; // The progress of the loading bar in fraction 0 to 1
     }
     
+    /**
+    * Update the progress of the loading bar
+    */
     update () {
-        this.progress += this.speed;
+        this.progress += this.speed; // Increment the progress by the speed
     }
    
-   
+    /**
+    * Convert the loading bar to a string representation
+    * @returns {string} The string representation of the loading bar
+    */
     toString () {
-        let bars = Math.floor(this.progress / 100 * 15);
+        let bars = Math.floor(this.progress * 15);
 
         let string = "";
         for (let i = 0; i < 15; i++) {
-            if (i <= bars) string += "#";
-            else string += ".";
+            if (i <= bars) string += bgWhite(white("#")); // For filled bars
+            else string += bgGray(gray(".")); // For empty bars
         }
-        return "[" + yellow(string) + "]";
+        return "[" + string + "]"; // Return the string representation with colors
     }
 }
 
